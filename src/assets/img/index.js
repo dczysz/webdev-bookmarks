994cca2141e776368c822e9d3734a961e211f7a4
D. Czysz <=>
2019-08-01 20:08:44 -0600

    Added images

---
+++ src/assets/img/index.js
@@ -0,0 +1,132 @@
+/*
+  To add image:
+    1. Open mobile view in FireFox (Ctrl + Shift + M)
+    2. Adjust screen size to roughly 1400x900
+    3. Take screenshot
+    4. Resize to 700px width
+    5. Save in /assets/img/ with hyphenated-name
+    6. Add import and export in this file and set as `url` property in data.js
+*/
+
+// Colors
+import coolors from './coolors.png';
+import colorHunt from './color-hunt.png';
+import colorSpace from './color-space.png';
+
+// Images
+import undraw from './undraw.png';
+import ezgif from './ezgif.png';
+import imageColor from './image-color.png';
+import humaaans from './humaaans.png';
+import fontAwesome from './font-awesome.png';
+import svgRepo from './svg-repo.png';
+
+// Backgrounds
+import coolBackgrounds from './cool-backgrounds.png';
+import heroPatterns from './hero-patterns.png';
+import svgBackgrounds from './svg-backgrounds.png';
+
+// HTML
+import linkRels from './link-rels.png';
+import metaTags from './meta-tags.png';
+
+// CSS
+import cssfx from './cssfx.png';
+import cssEffectsSnippets from './css-effects-snippets.png';
+import textInputEffects from './text-input-effects.png';
+import cssTricksGrid from './css-tricks-grid.png';
+import cssTricksDate from './css-tricks-date.png';
+import cssTricksFlexbox from './css-tricks-flexbox.png';
+import webDevTrick from './web-dev-trick.png';
+import hamburgers from './hamburgers.png';
+import everyLayout from './every-layout.png';
+
+// JavaScript
+import modernJs from './modern-js.png';
+import dripOfJs from './drip-of-js.png';
+import jsTips from './js-tips.png';
+import alligator from './alligator.png';
+import regex101 from './regex101.png';
+
+// APIs
+import countApi from './count-api.png';
+import publicApis from './public-apis.png';
+import randomUser from './random-user.png';
+import apiList from './api-list.png';
+import newsApi from './news-api.png';
+import jsonPlaceholder from './json-placeholder.png';
+import loremPicsum from './lorem-picsum.png';
+import placeholder from './placeholder.png';
+
+// Inspiration
+import andrewWangHoyer from './andrew-wang-hoyer.png';
+import danielTemkin from './daniel-temkin.png';
+import hakimElHattab from './hakim-el-hattab.png';
+import leaVerou from './lea-verou.png';
+import lokeshDhakar from './lokesh-dhakar.png';
+import sarahDrasner from './sarah-drasner.png';
+import gillesVermeulen from './gilles-vermeulen.png';
+import marc from './marc.png';
+import seanDorr from './sean-dorr.png';
+import httpster from './httpster.png';
+import mfwDesign from './mfw-design.png';
+import userInyerface from './user-inyerface.png';
+import checklistDesign from './checklist-design.png';
+import molamil from './molamil.png';
+import stripe from './stripe.png';
+import newlytics from './newlytics.png';
+
+export const images = {
+  coolors,
+  colorHunt,
+  colorSpace,
+  undraw,
+  ezgif,
+  imageColor,
+  humaaans,
+  fontAwesome,
+  svgRepo,
+  coolBackgrounds,
+  heroPatterns,
+  svgBackgrounds,
+  linkRels,
+  metaTags,
+  cssfx,
+  cssEffectsSnippets,
+  textInputEffects,
+  cssTricksGrid,
+  cssTricksDate,
+  cssTricksFlexbox,
+  webDevTrick,
+  hamburgers,
+  everyLayout,
+  modernJs,
+  dripOfJs,
+  jsTips,
+  alligator,
+  regex101,
+  countApi,
+  publicApis,
+  randomUser,
+  apiList,
+  newsApi,
+  jsonPlaceholder,
+  loremPicsum,
+  placeholder,
+  andrewWangHoyer,
+  danielTemkin,
+  hakimElHattab,
+  leaVerou,
+  lokeshDhakar,
+  sarahDrasner,
+  gillesVermeulen,
+  marc,
+  seanDorr,
+  httpster,
+  mfwDesign,
+  userInyerface,
+  checklistDesign,
+  molamil,
+  stripe,
+  newlytics,
+};
